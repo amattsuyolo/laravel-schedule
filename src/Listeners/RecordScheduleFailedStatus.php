3381a686d5c5a269f57af69697c480475c5f360d
matt <mattsuyolo@gmail.com>
2022-02-06 20:51:38 +0800

    新增record schedule failed listener

---
+++ src/Listeners/RecordScheduleFailedStatus.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MattSu\ScheduleAssistant\Listeners;
+
+use Illuminate\Console\Events\ScheduledTaskFailed;
+use Illuminate\Console\Events\ScheduledTaskFinished;
+use Illuminate\Console\Events\ScheduledTaskSkipped;
+use Illuminate\Console\Events\ScheduledTaskStarting;
+use MattSu\ScheduleAssistant\models\ScheduledAssistant;
+
+class RecordScheduleFailedStatus
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\OrderShipped  $event
+     * @return void
+     */
+    public function handle(ScheduledTaskFailed $event)
+    {
+        // Access the order using $event->order...
+        // $this->task = $task;
+        // $this->runtime = $runtime;
+        logger("我是RecordScheduleFailedStatus listener command:");
+        logger($event->task->command);
+        logger("我是RecordScheduleFailedStatus listener expression:");
+        logger($event->task->expression);
+        logger("我是RecordScheduleFailedStatus listener exception:");
+        logger($event->exception);
+        logger("我是RecordScheduleFailedStatus listener notTrack:");
+        logger($event->task->notTrack);
+        logger("我是RecordScheduleFailedStatus listener upperLimitsOfNormalMinutes:");
+        logger($event->task->upperLimitsOfNormalMinutes);
+        //Schedule 下 Withoutoverlapping 如果有排程死掉，
+        //但是排程手動下能正常執行時，
+        //到資料表scheduled_events找出key然後可以利用 php artisan tinker 直接下Cache::forget($mutex_cache_key);
+        // $mutex_cache_key = 'framework' . DIRECTORY_SEPARATOR . 'schedule-' . sha1($event->expression . $event->command);
+        $command = substr($event->task->command, strpos($event->task->command, 'artisan') + strlen('artisan') + 1);
+        $scheduledAssistant = ScheduledAssistant::create([
+            'type' => 'starting',
+            'command' => $command,
+            'logged_at' => date("Y-m-d H:i:s")
+        ]);
+    }
+}
