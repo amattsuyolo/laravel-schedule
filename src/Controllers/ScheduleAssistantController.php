ed3a3f0cf5415d07e8f29e8a218c5fb73cfd83b2
matt <mattsuyolo@gmail.com>
2022-02-17 20:41:16 +0800

    update

--- src/Controllers/ScheduleAssistantController.php
+++ src/Controllers/ScheduleAssistantController.php
@@ -4,11 +4,14 @@ namespace MattSu\ScheduleAssistant\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MattSu\ScheduleAssistant\models\ScheduledAssistant;
 
 class ScheduleAssistantController extends Controller
 {
     public function dashBoard()
     {
+        $ScheduledAssistant = ScheduledAssistant::all();
+        var_dump($ScheduledAssistant);
         return view('mattsu::scheduleDashboard');
     }
 }
