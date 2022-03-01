949ed88928b756f4860216544f58ff4ba8d6741f
matt <mattsuyolo@gmail.com>
2022-03-02 06:40:06 +0800

    feat(controller) adjust dashBoardIndex

--- src/Controllers/ScheduleAssistantController.php
+++ src/Controllers/ScheduleAssistantController.php
@@ -14,11 +14,43 @@ class ScheduleAssistantController extends Controller
      */
     public function dashBoardIndex()
     {
-        $ScheduledAssistant = ScheduledAssistant::where('type', 'starting')
+        $ScheduledAssistants = ScheduledAssistant::where('type', 'starting')
             ->orderBy("id", "desc")
             ->get()
             ->unique('command');
-        return view('mattsu::scheduleDashboard', ['data' => $ScheduledAssistant]);
+        foreach ($ScheduledAssistants as $item) {
+            $item->state = "default";
+            $item->msg = "";
+            //情況一 已經啟動 但沒有結束
+            $ScheduledAssistant = ScheduledAssistant::where('type', 'finish')
+                ->where('uuid', $item->uuid)
+                ->first();
+
+            $finish_logged_at = $ScheduledAssistant->logged_at ?? "";
+            if (!empty($ScheduledAssistant)) {
+                $item->state = "success";
+                $item->msg = "normal";
+            } else {
+                $dateTime = new \DateTime($item->logged_at);
+                $upperLimitsOfNormalMinutes = $item->upperLimitsOfNormalMinutes;
+                if (empty($upperLimitsOfNormalMinutes)) {
+                    $upperLimitsOfNormalMinutes = 10;
+                }
+                $dateTime->modify("+" . $upperLimitsOfNormalMinutes . " minutes");
+                if (now() > $dateTime) {
+                    $item->state = "error";
+                    $item->msg = "run too long";
+                }
+            }
+            //情況二 應該要啟動 但沒有啟動 (處理 next_run_at)
+            // 現在時間  > 如果紀錄start的next_run_at 	inactivated
+            if (date('Y-m-d H:i:s') > $item->nextRunAt) {
+                $item->state = "error";
+                $item->msg = "inactivated";
+            }
+        }
+
+        return view('mattsu::scheduleDashboardIndex', ['data' => $ScheduledAssistants]);
     }
     /**
      * 詳細歷史資訊
